2183702acdd05cbc33034e1513e483b2c948a294
smishra38@sapient.com <smishra38@sapient.com>
2017-07-27 21:43:55 +0530

    Moving data to an external file

---
+++ src/data.js
@@ -0,0 +1,43 @@
+var data = 
+[
+	{
+		question: "What is the Javscript compiler name in Google Chrome?",
+		options: ["Chrome V6", "Chrome V7","Chrome V8","Chrome V9"],
+		key: "Chrome V8"
+	},
+	{
+		question: "How can you detect the client's browser name?",
+		options: ["navigator.userAgent", "navigator.browser","browser.appName" ,"app.browserName"],
+		key: "navigator.userAgent"
+	},
+	{
+		question: "Nearly all objects in JavaScript are instances of which of the following?",
+		options: ["Object", "_proto_","Prototypes" ,"DOM"],
+		key: "Object"
+	},
+	{
+		question: "Which of the following is a correct way to empty an array?",
+		options: ["arrayName.empty();", "arrayName.splice(0, arrayList.length);","arrayName = null;" ,"arrayName = Obejct.empty();"],
+		key: "arrayName.splice(0, arrayList.length);"
+	},
+	{
+		question: "Which of the following is not a React Component Lifecycle method?",
+		options: ["componentWillUpdate ", "componentDidUpdate ","componentWillUnmount " ,"componentGoingToUpdate"],
+		key: "componentGoingToUpdate"
+	},
+	{
+		question: "In Experience Technology world, What does MEAN stand for?",
+		options: [	"Mongodb, ES6, Angularjs and Node",
+					"Mongodb, Ember, Angularjs and Node" ,
+					"Mongodb, Express, Angularjs and Node",
+					"Meteor, Express, Angularjs and Node"],
+		key: 		"Mongodb, Express, Angularjs and Node"
+	},
+	{
+		question: "In which of the following React Component Lifecycle methods, React refs do not work?",
+		options: ["componentWillMount", "componentDidMount ","componentWillUnmount " ,"componentDidUpdate"],
+		key: "componentWillMount"
+	}
+];
+
+module.exports = data;
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import PropTypes from 'prop-types';
+var data = require('./data.js');
 
-function shuffleArray(array) {
+var shuffleArray = array => {
 	let i = array.length - 1;
 	for (; i > 0; i--) {
 		const j = Math.floor(Math.random() * (i + 1));
@@ -15,13 +16,12 @@ function shuffleArray(array) {
 class Quiz extends React.Component{
 	constructor(props, context) {
 	    super(props, context);
-	    this.refreshQuestion = this.refreshQuestion.bind(this)
+	    this.refreshQuestion = this.refreshQuestion.bind(this);
 	    this.state = {
 	    	change:false
 	    }
 	}
 	refreshQuestion(){
-		console.log(this);
 		this.setState({
 			change:true
 		})
@@ -96,42 +96,6 @@ class Answer extends React.Component{
 	}
 
 }
-
-var data = [
-	{
-		question: "What is the Javscript compiler name in Google Chrome?",
-		options: ["Chrome V6", "Chrome V7","Chrome V8","Chrome V9"],
-		key: "Chrome V8"
-	},
-	{
-		question: "How can you detect the client's browser name?",
-		options: ["navigator.userAgent", "navigator.browser","browser.appName" ,"app.browserName"],
-		key: "navigator.userAgent"
-	},
-	{
-		question: "Nearly all objects in JavaScript are instances of which of the following?",
-		options: ["Object", "_proto_","Prototypes" ,"DOM"],
-		key: "Object"
-	},
-	{
-		question: "Which of the following is a correct way to empty an array?",
-		options: ["arrayName.empty();", "arrayName.splice(0, arrayList.length);","arrayName = null;" ,"arrayName = Obejct.empty();"],
-		key: "arrayName.splice(0, arrayList.length);"
-	},
-	{
-		question: "Which of the following is not a React Component Lifecycle method?",
-		options: ["componentWillUpdate ", "componentDidUpdate ","componentWillUnmount " ,"componentGoingToUpdate"],
-		key: "componentGoingToUpdate"
-	},
-	{
-		question: "In Experience Technology world, What does MEAN stand for?",
-		options: [	"Mongodb, ES6, Angularjs and Node",
-					"Mongodb, Ember, Angularjs and Node" ,
-					"Mongodb, Express, Angularjs and Node",
-					"Meteor, Express, Angularjs and Node"],
-		key: 		"Mongodb, Express, Angularjs and Node"
-	}
-]
 
 // Quiz.PropTypes = {
 // 	books: PropTypes.bool.isRequired,
